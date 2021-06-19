97f17c00b99cc36a04875edf968af28045d1c0df
Jonhks <jonhgx@gmail.com>
2021-06-19 02:01:34 -0500

    Update/creando solucion

--- src/App.js
+++ src/App.js
@@ -1,9 +1,73 @@
+import React, { useState, useEffect } from 'react'
+import Pregunta from './Components/Pregunta'
+import Formulario from './Components/Formulario'
+import Listado from './Components/Listado'
+import ControlPresupuesto from './Components/ControlPresupuesto'
 
-const App = () =>  {
+function App() {
+
+  const [presupuesto, setPresupuesto] = useState(0)
+  const [restante, setRestante] = useState(0)
+  const [showPregunta, setShowPregunta] = useState(true)
+  const [gastos, setGastos] = useState([])
+  const [gasto, setGasto] = useState({})
+  const [crearGasto, setCrearGasto] = useState(false)
+
+
+
+  // Actualizando el restante
+  useEffect(() => {
+    if(crearGasto) {
+      // agrega el nuevo presupuesto 
+      setGastos([
+        ...gastos,
+        gasto
+      ])
+      // restar del presupuesto
+      const presupuestoRestante = restante - gasto.cantidad
+      setRestante(presupuestoRestante)
+      // resetear a false 
+      setCrearGasto(false)
+    }
+  }, [gasto,gastos, crearGasto, restante])
+
+  // Cuando se genere un nuevo gasto
+  // const addNewGasto = gasto => {
+    
+  // }
 
   return (
-    <div>
-      <h1>Hola Chicas!!</h1>
+    <div className="container">
+      <header>
+        <h1>Gasto semanal</h1>
+        <div className="contenido-principal contenido">
+          {showPregunta ? (
+          <Pregunta 
+            setPresupuesto={setPresupuesto}
+            setRestante={setRestante}
+            setShowPregunta={setShowPregunta}
+          />
+          ):( 
+          <div className="row">
+            <div className="one-half column">
+              <Formulario
+                setGasto={setGasto}
+                setCrearGasto={setCrearGasto}
+              />
+            </div>
+            <div className="one-half column">
+              <Listado 
+                gastos={gastos} 
+              />
+              <ControlPresupuesto 
+                presupuesto={presupuesto}
+                restante={restante}
+              />
+            </div>
+          </div>
+          )}
+        </div>
+      </header>
     </div>
   )
 }
